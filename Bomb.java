06b4249095e2df3b999a04500f6951de678e9bb7
Xanatiz <henrik.thoreson@gmail.com>
2013-09-29 18:42:26 +0200

    cakeheadlol

--- Bomb.java
+++ Bomb.java
@@ -16,16 +16,34 @@ public class Bomb {
     private Playfield frontPlayfield;
     private Playfield background;
     private Player player;
+    private Position position;
 
-    public Bomb(int xChord, int yChord, int explosionRadius, Playfield frontPlayfield, Playfield background, Player player){
-        this.xPos = xChord;
-        this.yPos = yChord;
+    // Bomb får ta in x- och y-koordinater istället för position. kanske går med position också men då blir player helt fuckad. lätt fixat kanske men nu är det såhär
+    public Bomb(int xPos, int yPos, int explosionRadius, Playfield frontPlayfield, Playfield background, Player player){
+        this.xPos = xPos;
+        this.yPos = yPos;
         this.explosionRadius = explosionRadius;
         this.frontPlayfield=frontPlayfield;
         this.background=background;
         this.player=player;
-
     }
+
+   /* public int getBombXPosition(){
+        return this.position.getX();
+    }
+
+    public int getBombYPosition(){
+        return this.position.getY();
+    }*/
+
+    public int getxPos(){
+        return this.xPos;
+    }
+
+    public int getyPos(){
+        return this.yPos;
+    }
+
 
     public void activateBomb(){
         final Action explode = new AbstractAction() {
@@ -37,40 +55,78 @@ public class Bomb {
         frontPlayfield.setData(yPos, xPos, BlockType.BOMB);
         timer.setCoalesce(true);
         timer.start();
-
     }
 
     public void bang(){
         timer.stop();
         player.deactivateBomb();
         frontPlayfield.setData(yPos, xPos, null);
+        if(!player.isWithinKillZone(this)){                 // Här ligger checken för tillfället.
+            for(int i = xPos; i>=xPos-explosionRadius; i--){
+                if ((background.getData(yPos, i).isDestructible())&&((frontPlayfield.getData(yPos, i)!=null) && (frontPlayfield.getData(yPos, i).isDestructible()))){
+                    frontPlayfield.setData(yPos, i, null);
+                    break;}
+                else if (!background.getData(yPos, i).isDestructible())
+                    break;
+            }
+            for(int i = xPos; i<=xPos+explosionRadius; i++){
+                if ((background.getData(yPos, i).isDestructible())&&((frontPlayfield.getData(yPos, i)!=null) && (frontPlayfield.getData(yPos, i).isDestructible()))){
+                    frontPlayfield.setData(yPos, i, null);
+                    break;}
+                else if (!background.getData(yPos, i).isDestructible())
+                    break;
+            }
+            for(int i = yPos; i>=yPos-explosionRadius; i--){
+                if ((background.getData(i, xPos).isDestructible())&&((frontPlayfield.getData(i, xPos)!=null) && (frontPlayfield.getData(i, xPos).isDestructible()))){
+                    frontPlayfield.setData(i, xPos, null);
+                    break;}
+                else if (!background.getData(i, xPos).isDestructible())
+                    break;
+            }
+            for(int i = yPos; i<=yPos+explosionRadius; i++){
+                if ((background.getData(i, xPos).isDestructible())&&((frontPlayfield.getData(i, xPos)!=null) && (frontPlayfield.getData(i, xPos).isDestructible()))){
+                    frontPlayfield.setData(i, xPos, null);
+                    break;}
+                else if (!background.getData(i, xPos).isDestructible())
+                    break;
+            }
+        }
+    }
+
+    public boolean checkVicinity(){
         for(int i = xPos; i>=xPos-explosionRadius; i--){
             if ((background.getData(yPos, i).isDestructible())&&((frontPlayfield.getData(yPos, i)!=null) && (frontPlayfield.getData(yPos, i).isDestructible()))){
-                frontPlayfield.setData(yPos, i, null);
-                break;}
+                return true;
+                //frontPlayfield.setData(yPos, i, null);
+                }
             else if (!background.getData(yPos, i).isDestructible())
-                break;
+                return false;
         }
         for(int i = xPos; i<=xPos+explosionRadius; i++){
             if ((background.getData(yPos, i).isDestructible())&&((frontPlayfield.getData(yPos, i)!=null) && (frontPlayfield.getData(yPos, i).isDestructible()))){
-                frontPlayfield.setData(yPos, i, null);
-                break;}
+                return true;
+                //frontPlayfield.setData(yPos, i, null);
+                }
             else if (!background.getData(yPos, i).isDestructible())
-                break;
+                return false;
         }
         for(int i = yPos; i>=yPos-explosionRadius; i--){
             if ((background.getData(i, xPos).isDestructible())&&((frontPlayfield.getData(i, xPos)!=null) && (frontPlayfield.getData(i, xPos).isDestructible()))){
-                frontPlayfield.setData(i, xPos, null);
-                break;}
+                return true;
+                //frontPlayfield.setData(i, xPos, null);
+                }
             else if (!background.getData(i, xPos).isDestructible())
-                break;
+                return false;
         }
         for(int i = yPos; i<=yPos+explosionRadius; i++){
             if ((background.getData(i, xPos).isDestructible())&&((frontPlayfield.getData(i, xPos)!=null) && (frontPlayfield.getData(i, xPos).isDestructible()))){
-                frontPlayfield.setData(i, xPos, null);
-                break;}
+                return true;
+                //frontPlayfield.setData(i, xPos, null);
+                }
             else if (!background.getData(i, xPos).isDestructible())
-                break;
+                return false;
         }
+        return false;
+
     }
 }
--- Player.java
+++ Player.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+import java.math.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -8,12 +10,13 @@
  */
 public class Player {
 
-    public int standardExplosionRadius = 3;
+    private int standardExplosionRadius = 3;
     private Playfield background;
     private Playfield frontPlayfield;
     private Position position;
     private int explosionRadius;
-    private boolean activeBomb;
+    public boolean activeBomb;
+    final JFrame frame = new JFrame();
 
     public Player(Position position, Playfield background, Playfield frontPlayfield) {
         this.position = position;
@@ -27,21 +30,38 @@ public class Player {
         return position;
     }
 
+    public int getXPosition(){
+        return position.getX();
+    }
+
+    public int getYPosition(){
+        return position.getY();
+    }
 
     public void moveRight(){
         position.nextRight(background, frontPlayfield);
+        //System.out.println("Current X: " + getXPosition());
+        //System.out.println("Current Y: " + getYPosition());
+
     }
 
     public void moveLeft(){
         position.nextLeft(background, frontPlayfield);
+        //System.out.println("Current X: " + getXPosition());
+        //System.out.println("Current Y: " + getYPosition());
     }
 
     public void moveUp(){
         position.nextUp(background, frontPlayfield);
+        //System.out.println("Current X: " + getXPosition());
+        //System.out.println("Current Y: " + getYPosition());
+
     }
 
     public void moveDown(){
         position.nextDown(background, frontPlayfield);
+        //System.out.println("Current X: " + getXPosition());
+        //System.out.println("Current Y: " + getYPosition());
     }
 
     public void dropBomb(){
@@ -49,9 +69,44 @@ public class Player {
             Bomb bomb = new Bomb(position.getX(), position.getY(), explosionRadius, frontPlayfield, background, this);
             activeBomb=true;
             bomb.activateBomb();
+
         }
     }
     public void deactivateBomb(){
         activeBomb=false;
+    }
+
+    // Lade in dessa tre metoder. Simpla. Första två kollar bara om bomben och spelares nuvarande position är i samma rad/kolumn.
+    // Tredje metoder kollar bara om bombens X - spelares X <= explosionRadius(3) samt om de är i samma kolumn eller rad.
+    // likadant för Y-positionerna.
+
+    public boolean sameRow(Bomb bomb){
+        return this.getYPosition() == bomb.getyPos();
+    }
+
+    public boolean sameColumn(Bomb bomb){
+        return this.getXPosition() == bomb.getxPos();
+    }
+
+    public boolean isWithinKillZone(Bomb bomb){
+        try {
+            if((((bomb.getxPos() - this.getXPosition() <= explosionRadius) && (bomb.getyPos() - this.getYPosition() <= explosionRadius)) && ((sameColumn(bomb)||(sameRow(bomb))))) && !bomb.checkVicinity()){
+                int answer = JOptionPane.showConfirmDialog(frame, "new game?"); //"New game?", JOptionPane.YES_NO_OPTION);
+                //System.out.println("WITHIN KILL ZONE U DED BICH");
+                //System.exit(0);
+                if(answer == JOptionPane.YES_OPTION){
+                    new Menu();
+                }
+                else if(answer == JOptionPane.NO_OPTION) {
+                    System.exit(0);
+                }
+                return true;
+            }
+        }
+        catch (NullPointerException e){
+            System.out.println("NULL POINT");
+        }
+        return false;
+
     }
 }
