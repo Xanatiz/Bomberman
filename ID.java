d8a239306a1e7708df2ac1c266acc937b7350f51
Xanatiz <henrik.thoreson@gmail.com>
2013-10-02 12:24:52 +0200

    Added ID-class.

---
+++ ID.java
@@ -0,0 +1,19 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: henke
+ * Date: 10/2/13
+ * Time: 11:23 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ID {
+
+    private int id;
+
+    public ID(int id ){
+        this.id=id;
+    }
+
+    public int read(){
+        return id;
+    }
+}
